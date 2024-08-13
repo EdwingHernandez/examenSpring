a03d1b9fee6882af92c118913cc22c537b63890f
EdwingHernandez <124322869+EdwingHernandez@users.noreply.github.com>
2024-08-13 10:14:46 -0500

    feat :construction: Se crea el controlador

--- springproject/src/main/java/com/campus/springproject/persistence/entities/Customer.java
+++ springproject/src/main/java/com/campus/springproject/persistence/entities/Customer.java
@@ -3,6 +3,7 @@ package com.campus.springproject.persistence.entities;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -13,6 +14,7 @@ import jakarta.persistence.Table;
 @Table(name = "customer")
 public class Customer {
 
+    @Id
     private String idcustomer;
     private String namecustomer;
     private String lastnamecustomer;
--- springproject/src/main/java/com/campus/springproject/web/controllers/FarmacyController.java
+++ springproject/src/main/java/com/campus/springproject/web/controllers/FarmacyController.java
@@ -1,13 +1,32 @@
 package com.campus.springproject.web.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.campus.springproject.domain.services.FarmacyService;
+import com.campus.springproject.persistence.entities.Farmacy;
+
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/farmacy")
 public class FarmacyController {
 
-    
+    @Autowired
+    private FarmacyService farmacyService;
 
+    public FarmacyController(FarmacyService farmacyService) {
+        this.farmacyService = farmacyService;
+    }
+
+
+    @GetMapping("/all")
+    public List<Farmacy> findAllfarmacy(){
+
+        return farmacyService.findAll();
+    }
+    
 
 }
